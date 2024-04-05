815d4d5724f5bec0803e87b076efc5e51d61732e
Priyanshu <vishwakarmapriyanshu98@gmail.com>
2024-04-05 21:23:15 +0530

    1:30 completed

--- Backend/Controllers/message.controller.js
+++ Backend/Controllers/message.controller.js
@@ -1,11 +1,67 @@
+import Conversation from '../models/conversation.model.js';
+import Message from '../models/message.model.js';
 export const sendMessage = async (req, res) => {
    try {
     
     const {message} = req.body;
-    const {id} = req.params;
-    const senderid = req.user._id
+    const {id: recieverId} = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+          participants: {$all: [senderId, recieverId]},
+    })
+
+      if(!conversation){
+         conversation = await Conversation.create({
+            participants: [senderId, recieverId],
+         })
+      }
+
+     const newMessage = new Message({
+       senderId,
+       recieverId,
+       message,
+     });
+
+      if(newMessage){
+         conversation.messages.push(newMessage._id);
+      }
+
+      // SOCKET TO FUNCTIONALITY WILL GO HERE
+
+      // await conversation.save();
+      // await newMessage.save();
+
+
+      // this will run in parellel
+      await Promise.all([  conversation.save(), newMessage.save() ]);
+
+      res.status(200).json(newMessage);
+
    } catch (error) {
     console.log("Error in SendMessage controller", error.message)
-    res.ststus(500).json({error: "Internal server error"});
+    res.status(500).json({error: "Internal server error"});
    }
 };
+
+export const getMessages = async(req,res) =>{
+
+   try {
+
+      const {id:userTochatId}= req.params;
+      const senderId = req.user._id;
+
+      const conversation = await Conversation.findOne({
+         participants: { $all: [ senderId, userTochatId] },
+      }).populate("messages");
+
+      if(!conversation) return res.status(200).json([]);
+      const messages = conversation.messages;
+
+      res.status(200).json(messages);
+      
+   } catch (error) {
+      console.log("Error in getMessage controller", error.message)
+      res.status(500).json({error: "Internal server error"});
+   }
+}
