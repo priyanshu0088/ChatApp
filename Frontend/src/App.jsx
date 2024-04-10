3a3a91518755725bce9419e4c1f2128bf65f629c
Priyanshu <vishwakarmapriyanshu98@gmail.com>
2024-04-10 22:20:01 +0530

    2:01 COMPLETED

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,13 @@
 
 import './App.css'
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
 
 function App() {
   return (
-    <>
-    <button class="btn">Button</button>
-<button class="btn btn-neutral">Neutral</button>
-<button class="btn btn-primary">Primary</button>
-<button class="btn btn-secondary">Secondary</button>
-<button class="btn btn-accent">Accent</button>
-<button class="btn btn-ghost">Ghost</button>
-<button class="btn btn-link">Link</button>
-    </>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <SignUp/>
+    </div>
   )
 }
 
