838dd816d89d35a744c7daa4066699a83671e670
Priyanshu <vishwakarmapriyanshu98@gmail.com>
2024-04-14 02:01:26 +0530

    2:25

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,12 @@
 import './App.css'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
+import Home from './pages/home/Home'
 
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <SignUp/>
+     <Home/>
     </div>
   )
 }
---
+++ Frontend/src/components/messages/Message.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div className='chat chat-end'>
+    <div className='chat-image avatar'>
+        <div className='w-10 rounded-full'>
+            <img alt='Tailwind CSS chat bubble component' src={"https://avatar.iran.liara.run/public/boy?username=Scott"} />
+        </div>
+    </div>
+    <div className={`chat-bubble text-white bg-blue-500`}>hey whatsapp!!</div>
+    <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:30</div>
+</div>
+  )
+}
+
+export default Message
