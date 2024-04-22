9eb0072c8e49913ec3271c5ab79792bb0bb6c787
Priyanshu <vishwakarmapriyanshu98@gmail.com>
2024-04-22 18:07:18 +0530

    3:58 completed

--- Frontend/src/components/messages/Messages.jsx
+++ Frontend/src/components/messages/Messages.jsx
@@ -1,18 +1,26 @@
 import Message from "./Message";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../sidebar/MessageSkeleton";
+import { useEffect,useRef } from "react";
 const Messages = () => {
   const { messages, loading } = useGetMessages();
-  console.log("Messages", messages);
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+		setTimeout(() => {
+			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+		}, 100);
+	}, [messages]);
+
   return (
-    <div className="px-4 flex-1 overflow-auto">
-      {!loading &&
-        messages.length > 0 &&
-        messages.map((message) => (
-          <div key={message._id}>
-            <Message message={message} />
-          </div>
-        ))}
+    <div className='px-4 flex-1 overflow-auto'>
+    {!loading &&
+      messages.length > 0 &&
+      messages.map((message) => (
+        <div key={message._id} ref={lastMessageRef}>
+          <Message message={message} />
+        </div>
+      ))}
 
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
       {!loading && messages.length === 0 && (
